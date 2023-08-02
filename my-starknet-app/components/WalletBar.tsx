b3c643715d05d742c516490e8d2c676ff17d7ea7
Daniel Bejarano <dbejarano820@gmail.com>
2023-08-02 13:07:06 -0600

    WalletBar refactor

--- my-starknet-app/components/WalletBar.tsx
+++ my-starknet-app/components/WalletBar.tsx
@@ -1,8 +1,14 @@
 import { useAccount, useConnectors } from '@starknet-react/core'
-import { useMemo } from 'react'
+import { useMemo, useState } from 'react'
+import Button from '@mui/material/Button'
+import Dialog from '@mui/material/Dialog'
+import DialogActions from '@mui/material/DialogActions'
+import DialogContent from '@mui/material/DialogContent'
+import DialogContentText from '@mui/material/DialogContentText'
+import DialogTitle from '@mui/material/DialogTitle'
+import { useTheme } from '@mui/material/styles'
 
-function WalletConnected() {
-  const { address } = useAccount()
+function WalletConnected({ address } : { address: string }) {
   const { disconnect } = useConnectors()
 
   const shortenedAddress = useMemo(() => {
@@ -12,25 +18,56 @@ function WalletConnected() {
 
   return (
     <div>
-      <span>Connected: {shortenedAddress}</span>
-      <button onClick={disconnect}>Disconnect</button>
+      <Button color="inherit" onClick={disconnect}>
+        {shortenedAddress}
+      </Button>
+      <span>Connected to Mainnet</span>
     </div>
   )
 }
 
 function ConnectWallet() {
   const { connectors, connect } = useConnectors()
+  const [open, setOpen] = useState(false)
+  const theme = useTheme()
+
+  const handleClickOpen = () => {
+    setOpen(true)
+  }
+
+  const handleClose = () => {
+    setOpen(false)
+  }
 
   return (
     <div>
-      <span>Choose a wallet:</span>
-      {connectors.map((connector) => {
-        return (
-          <button key={connector.id} onClick={() => connect(connector)}>
-            {connector.id}
-          </button>
-        )
-      })}
+      <Button color="inherit" onClick={handleClickOpen}>
+        Connect Wallet
+      </Button>
+      <Dialog open={open} onClose={handleClose}>
+        <DialogTitle>Choose a wallet:</DialogTitle>
+        <DialogContent>
+          <DialogContentText>
+            {connectors.map((connector) => (
+              <Button
+                key={connector.id}
+                onClick={() => {
+                  connect(connector)
+                  handleClose()
+                }}
+                sx={{ margin: theme.spacing(1) }}
+              >
+                {connector.id}
+              </Button>
+            ))}
+          </DialogContentText>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleClose} color="inherit">
+            Cancel
+          </Button>
+        </DialogActions>
+      </Dialog>
     </div>
   )
 }
@@ -38,5 +75,5 @@ function ConnectWallet() {
 export default function WalletBar() {
   const { address } = useAccount()
 
-  return address ? <WalletConnected /> : <ConnectWallet />
+  return address ? <WalletConnected address={address} /> : <ConnectWallet />
 }
